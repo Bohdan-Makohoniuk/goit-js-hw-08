a2cb9dcb99d5cb42f976c335e5777c8ced491f11
Bohdan <makohoniuk.bo@gmail.com>
2022-09-08 20:12:04 +0300

    save localStorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+const feedback = document.querySelector('.feedback-form');
+const formData = {};
+function handleInput(event) {
+    const { name, value } = event.target;
+    formData[name] = value;
 
+    const formDataJSON = JSON.stringify(formData);
+
+    localStorage.setItem('formKey', formDataJSON)
+    console.log(formDataJSON);
+     console.log(formDataJSON);
+  
+}
+
+feedback.addEventListener('input', handleInput);
