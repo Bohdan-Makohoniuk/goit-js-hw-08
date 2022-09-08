e7c72949cd109f94c186b2fe0eca9f8726c0ba06
Bohdan <makohoniuk.bo@gmail.com>
2022-09-08 21:00:09 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,7 @@
 const feedback = document.querySelector('.feedback-form');
 const formData = {};
+initPage();
+
 function handleInput(event) {
     const { name, value } = event.target;
     formData[name] = value;
@@ -11,5 +13,19 @@ function handleInput(event) {
      console.log(formDataJSON);
   
 }
+function initPage (arguments) {
+    const savedData = localStorage.getItem('formKey');
+    if (!savedData) {
+        return;
+    }
+    const savedDataObj = JSON.parse(savedData);
+    // Масив масивів повертає Object.entries
+    Object.entries(savedDataObj).forEach(([name, value]) => {
+       
+    });
+    
+}
+
+
 
 feedback.addEventListener('input', handleInput);
